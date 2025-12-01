df0ccfb5ff7cc6c094b80b0f1ee64d1fd7e5f7ad
Silent0agent <deniskuz08@gmail.com>
2025-12-02 01:58:58 +0300

    feat: users email backend
    fix: users manager, add avatar validation

--- django_project/users/managers.py
+++ django_project/users/managers.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import UserManager
 
 class CustomUserManager(UserManager):
     def normalize_email(self, email):
+        if not email or "@" not in email:
+            return email
+
         email = super().normalize_email(email)
         email = email.lower()
         local_part, domain = email.split("@")
@@ -22,14 +25,11 @@ class CustomUserManager(UserManager):
         local_part = re.sub(r"\+.*", "", local_part)
         return f"{local_part}@{domain}"
 
-    def active(self):
-        return self.filter(is_active=True)
-
     def by_mail(self, email):
-        return self.active().filter(email=email).first()
+        return self.filter(email=email).first()
 
     def public_information(self):
-        return self.active().only(
+        return self.only(
             self.model.username.field.name,
             self.model.email.field.name,
             self.model.avatar.field.name,
