55aaea733a0f6600408976914a9695ab4a35bd10
ChelnokovEgor <etchelnockov@yandex.ru>
2025-12-02 20:47:03 +0300

    feature: базовая модель и приложение tasks

---
+++ django_project/tasks/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import TextTask
+
+
+@admin.register(TextTask)
+class TextTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client', 'created_at', 'updated_at')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'content', 'client__email')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('client', 'title', 'content')
+        }),
+        (_('Timestamps'), {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
---
+++ django_project/tasks/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
+
+class TextTask(models.Model):
+    """Модель текстового задания для проверки ИИ."""
+
+    client = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        verbose_name=_("client"),
+        related_name="text_tasks",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("created at"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("updated at"),
+    )
+    title = models.CharField(
+        max_length=255,
+        verbose_name=_("title"),
+    )
+    content = models.TextField(
+        verbose_name=_("content"),
+    )
+
+    class Meta:
+        verbose_name = _("text task")
+        verbose_name_plural = _("text tasks")
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
