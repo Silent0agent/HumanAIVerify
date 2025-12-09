56375c6619c8fd2ce66670217d021f735e07040d
Silent0agent <deniskuz08@gmail.com>
2025-12-09 20:11:22 +0300

    refactor: construct tasks file hierarchy

---
+++ django_project/tasks/forms/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "BaseTaskCheckForm",
+    "BaseTaskForm",
+    "TextTaskCheckForm",
+    "TextTaskForm",
+)
+
+from tasks.forms.base import BaseTaskCheckForm, BaseTaskForm
+from tasks.forms.text import TextTaskCheckForm, TextTaskForm
--- django_project/tasks/urls.py
+++ django_project/tasks/urls.py
@@ -2,8 +2,6 @@ __all__ = ()
 
 from django.urls import path
 
-import tasks.forms
-import tasks.models
 import tasks.views
 
 app_name = "tasks"
@@ -11,20 +9,12 @@ app_name = "tasks"
 urlpatterns = [
     path(
         "text-task/create/",
-        tasks.views.BaseTaskCreateView.as_view(
-            model=tasks.models.TextTask,
-            form_class=tasks.forms.TextTaskForm,
-        ),
+        tasks.views.TextTaskCreateView.as_view(),
         name="text-task-create",
     ),
     path(
         "text-task/<int:task_id>/check/",
-        tasks.views.BaseTaskCheckPerformView.as_view(
-            template_name="tasks/text_check_perform.html",
-            task_model=tasks.models.TextTask,
-            check_model=tasks.models.TextTaskCheck,
-            form_class=tasks.forms.TextTaskCheckForm,
-        ),
+        tasks.views.TextTaskCheckPerformView.as_view(),
         name="text-check-perform",
     ),
 ]
