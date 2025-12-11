e9acaa478d6c89930f183ab04222536fdd70de77
Silent0agent <deniskuz08@gmail.com>
2025-12-12 01:12:37 +0300

    feat: add image tasks

--- django_project/tasks/models/text.py
+++ django_project/tasks/models/text.py
@@ -1,14 +1,10 @@
 __all__ = ()
 
-from django.conf import settings
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-import core.models
 import tasks.fields
-import tasks.managers
-from tasks.models.base import BaseTask
+from tasks.models.base import BaseTask, BaseTaskCheck
 
 
 class TextTask(BaseTask):
@@ -22,58 +18,10 @@ class TextTask(BaseTask):
         verbose_name_plural = _("text_tasks")
 
 
-class TextTaskCheck(core.models.TimeStampedModel):
-    class Status(models.TextChoices):
-        DRAFT = "draft", _("draft")
-        PUBLISHED = "published", _("published")
+class TextTaskCheck(BaseTaskCheck):
+    task = tasks.fields.make_check_task_field(TextTask)
+    performer = tasks.fields.make_check_performer_field("text")
 
-    task = models.ForeignKey(
-        TextTask,
-        on_delete=models.CASCADE,
-        related_name="checks",
-        related_query_name="check",
-        verbose_name=_("task"),
-    )
-    performer = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.PROTECT,
-        related_name="task_checks",
-        related_query_name="task_check",
-        verbose_name=_("performer"),
-    )
-    ai_score = models.FloatField(
-        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
-        verbose_name=_("ai_score"),
-    )
-    comment = models.TextField(
-        blank=True,
-        verbose_name=_("comment"),
-    )
-    status = models.CharField(
-        max_length=15,
-        choices=Status.choices,
-        default=Status.DRAFT,
-        verbose_name=_("status"),
-    )
-
-    objects = tasks.managers.TaskCheckManager()
-
-    class Meta:
-        verbose_name = _("task_check")
-        verbose_name_plural = _("task_checks")
-        ordering = ["-created_at"]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["task", "performer"],
-                name="unique_task_performer",
-            ),
-        ]
-
-    def __str__(self):
-        task_title = self.task.title
-        if len(task_title) > 20:
-            task_title = task_title[:20] + "..."
-
-        return (
-            f"{self.task.title} | {self.performer.username} ({self.ai_score}%)"
-        )
+    class Meta(BaseTaskCheck.Meta):
+        verbose_name = _("text_task_check")
+        verbose_name_plural = _("text_task_checks")
