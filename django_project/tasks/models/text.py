e808682b565cfbc9e73babbd02f4092eae8adfa4
Denis <Silent0agent@yandex.ru>
2025-12-13 15:19:36 +0300

    feat: audio tasks

--- django_project/tasks/models/text.py
+++ django_project/tasks/models/text.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from django_ckeditor_5.fields import CKEditor5Field
 
 import tasks.fields
-from tasks.models import BaseTask, BaseTaskCheck
+from tasks.models.base import BaseTask, BaseTaskCheck
 
 
 class TextTask(BaseTask):
