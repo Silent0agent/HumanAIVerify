faa111cc8b73f42abaa9c5153c9d453ad2c9e855
Silent0agent <deniskuz08@gmail.com>
2025-12-16 23:22:59 +0300

    fix: bleach sanitizing

--- django_project/tasks/views/text.py
+++ django_project/tasks/views/text.py
@@ -61,8 +61,7 @@ class TextTaskCheckPerformView(BaseTaskCheckPerformView):
         raw_html = request.POST.get("highlighted_content")
 
         if raw_html:
-            # final_content = core.utils.sanitize_html(raw_html)
-            final_content = raw_html
+            final_content = core.utils.sanitize_html(raw_html)
         else:
             final_content = self.task.content
 
