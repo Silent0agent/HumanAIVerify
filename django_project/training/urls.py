fedb6df0562fab9dcb5f235a1dc85ff272007130
Silent0agent <deniskuz08@gmail.com>
2025-12-17 23:15:37 +0300

    fix: admin auth group model import

--- django_project/training/urls.py
+++ django_project/training/urls.py
@@ -2,16 +2,20 @@ __all__ = ()
 
 from django.urls import path
 
-from training import views
+import training.views
 
 app_name = "training"
 
 urlpatterns = [
-    path("start/", views.TrainingStartView.as_view(), name="start"),
+    path("start/", training.views.TrainingStartView.as_view(), name="start"),
     path(
         "take-test/<int:text_id>/",
-        views.TrainingTakeTestView.as_view(),
+        training.views.TrainingTakeTestView.as_view(),
         name="take-test",
     ),
-    path("results/", views.TrainingResultsView.as_view(), name="results"),
+    path(
+        "results/",
+        training.views.TrainingResultsView.as_view(),
+        name="results",
+    ),
 ]
