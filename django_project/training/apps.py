52f9d10793fa2d266515c9a7df1a62dd1d6a4291
Denis <Silent0agent@yandex.ru>
2025-12-17 21:41:25 +0300

    refactor: add training completions variable to env file

--- django_project/training/apps.py
+++ django_project/training/apps.py
@@ -1,8 +1,10 @@
 __all__ = ()
 
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class TrainingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "training"
+    verbose_name = _("Training_app_name")
