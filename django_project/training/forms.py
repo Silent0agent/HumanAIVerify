3c0f53f216db604310838844eb1e32bba11c831d
Denis <Silent0agent@yandex.ru>
2025-12-17 23:11:04 +0300

    fix: group model import

--- django_project/training/forms.py
+++ django_project/training/forms.py
@@ -5,11 +5,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TrainingTextForm(forms.Form):
-    is_ai_generated = forms.ChoiceField(
+    is_ai_generated = forms.TypedChoiceField(
         choices=[
             (True, _("AI_generated")),
             (False, _("Human_written")),
         ],
+        coerce=lambda x: str(x).lower() == "true",
         widget=forms.RadioSelect,
         label=_("Is_this_text_AI_generated?"),
     )
@@ -17,12 +18,3 @@ class TrainingTextForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.training_text = kwargs.pop("training_text", None)
         super().__init__(*args, **kwargs)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        is_ai_generated = cleaned_data.get("is_ai_generated")
-
-        if is_ai_generated is not None:
-            cleaned_data["is_ai_generated"] = is_ai_generated == "True"
-
-        return cleaned_data
